b120d4b6994ddd85324886a7aa5742dd860f3c7e
이하린 <gmlwls608@gmail.com>
2023-07-24 17:52:12 +0900

    Init : Avatar Entity 생성 #7

---
+++ Server/scrooge/src/main/java/com/scrooge/scrooge/user/domain/Avatar.java
@@ -0,0 +1,26 @@
+package com.scrooge.scrooge.user.domain;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "avatar")
+@Getter @Setter
+@NoArgsConstructor
+public class Avatar {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "avatar_id")
+    private Long id;
+
+    @Column(nullable = false, length = 255)
+    private String name;
+
+    @Column(nullable = false, length = 255)
+    private String img_address;
+
+}
