4d8253e9e360ce3bfbc721adf6caecc616ac7ab5
이하린 <gmlwls608@gmail.com>
2023-07-24 12:04:21 +0900

    Init : 백엔드 서버 환경 초기 세팅 #3

---
+++ Server/scrooge/src/test/java/com/scrooge/scrooge/ScroogeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scrooge.scrooge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScroogeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
