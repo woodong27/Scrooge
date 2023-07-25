28f45aa0ffa1427eb65d307660167386827873e4
이동주 <leednj0113@naver.com>
2023-07-25 17:57:45 +0900

    Style: Main #5

--- scoorge-react-app/src/components/Main/Main.js
+++ scoorge-react-app/src/components/Main/Main.js
@@ -6,23 +6,25 @@ import styles from "./Main.module.css";
 const Main = () => {
   return (
     <Fragment>
-      <ProgressBar />
-      <div className={styles.header34}>
-        <p>Lv. 3</p>
-        <p>돈그만써</p>
+      <div className={styles.card}>
+        <ProgressBar />
+        <div className={styles.header34}>
+          <p>Lv. 3</p>
+          <p>돈그만써</p>
+        </div>
+        <div className={styles.box}>
+          <img
+            className={styles.img}
+            src={`${process.env.PUBLIC_URL}/Character/${
+              ~~(Math.random() * 6) + 1
+            }.png`}
+            alt="캐릭터"
+          />
+        </div>
+        <Link to="/300">
+          <button className={styles.btn}>일일정산</button>
+        </Link>
       </div>
-      <div>
-        <img
-          className={styles.img}
-          src={`${process.env.PUBLIC_URL}/Character/${
-            ~~(Math.random() * 6) + 1
-          }.png`}
-          alt="캐릭터"
-        />
-      </div>
-      <Link to="/300">
-        <button className={styles.btn}>일일정산</button>
-      </Link>
     </Fragment>
   );
 };
