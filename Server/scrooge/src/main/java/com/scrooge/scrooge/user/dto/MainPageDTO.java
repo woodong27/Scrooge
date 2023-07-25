f5f2c58f505f22e35237c65a7d686b1f43e415e9
woodong27 <cdw5548@gmail.com>
2023-07-25 17:31:25 +0900

    Feat : Update MainPageDTO.java
    MainPageDTO에 mainAvartarId 추가
    @Setter annotation 추가

--- Server/scrooge/src/main/java/com/scrooge/scrooge/user/dto/MainPageDTO.java
+++ Server/scrooge/src/main/java/com/scrooge/scrooge/user/dto/MainPageDTO.java
@@ -1,10 +1,12 @@
 package com.scrooge.scrooge.user.dto;
 
 import com.scrooge.scrooge.user.domain.Level;
+import lombok.Setter;
 
-public class MainPageDto {
+@Setter
+public class MainPageDTO {
     private String nickname;
     private Level level;
     private int exp;
-    private Boolean isMainAvatar;
+    private Long mainAvtarId;
 }
