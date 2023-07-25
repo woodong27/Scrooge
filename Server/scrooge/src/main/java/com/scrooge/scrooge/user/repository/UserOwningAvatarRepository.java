ed0ae2d5cfd2883057ec251f9e1414588ab22c8c
woodong27 <cdw5548@gmail.com>
2023-07-25 17:33:02 +0900

    Feat : Update UserOwningAvatarRepository

--- Server/scrooge/src/main/java/com/scrooge/scrooge/user/repository/UserOwningAvatarRepository.java
+++ Server/scrooge/src/main/java/com/scrooge/scrooge/user/repository/UserOwningAvatarRepository.java
@@ -2,6 +2,11 @@ package com.scrooge.scrooge.user.repository;
 
 import com.scrooge.scrooge.user.domain.UserOwningAvatar;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
+@Repository
 public interface UserOwningAvatarRepository extends JpaRepository<UserOwningAvatar, Long> {
 }
