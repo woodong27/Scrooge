2ba806e23552ee13c040ac2f698d28752b26bc49
이하린 <gmlwls608@gmail.com>
2023-07-25 14:47:13 +0900

    Rename: Rename Android Project name #10

---
+++ scoorge-android/app/src/main/java/com/example/scoorge_android/MyNotificationListenerService.kt
@@ -0,0 +1,35 @@
+package com.example.scoorge_android
+
+import android.app.Notification
+import android.service.notification.NotificationListenerService
+import android.service.notification.StatusBarNotification
+import android.util.Log
+
+class MyNotificationListenerService : NotificationListenerService() {
+
+    private val TAG = "MyNotificationListenerService"
+
+    override fun onNotificationPosted(sbn: StatusBarNotification?) {
+        super.onNotificationPosted(sbn)
+        val packageName: String = sbn?.packageName ?: "Null"
+        val extras = sbn?.notification?.extras
+
+        val extraTitle: String = extras?.get(Notification.EXTRA_TITLE).toString()
+        val extraText: String = extras?.get(Notification.EXTRA_TEXT).toString()
+        val extraBigText: String = extras?.get(Notification.EXTRA_BIG_TEXT).toString()
+        val extraInfoText: String = extras?.get(Notification.EXTRA_INFO_TEXT).toString()
+        val extraSubText: String = extras?.get(Notification.EXTRA_SUB_TEXT).toString()
+        val extraSummaryText: String = extras?.get(Notification.EXTRA_SUMMARY_TEXT).toString()
+
+        Log.d(
+            TAG, "onNotificationPosted:\n" +
+                    "PackageName: $packageName" +
+                    "Title: $extraTitle\n" +
+                    "Text: $extraText\n" +
+                    "BigText: $extraBigText\n" +
+                    "InfoText: $extraInfoText\n" +
+                    "SubText: $extraSubText\n" +
+                    "SummaryText: $extraSummaryText\n"
+        )
+    }
+}
