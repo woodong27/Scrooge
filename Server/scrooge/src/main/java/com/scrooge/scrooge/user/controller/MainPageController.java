63e849d00643f94c41fe91419bf4b4831345afee
woodong27 <cdw5548@gmail.com>
2023-07-25 17:26:55 +0900

    Feat : Update MainPageController-#9

--- Server/scrooge/src/main/java/com/scrooge/scrooge/user/controller/MainPageController.java
+++ Server/scrooge/src/main/java/com/scrooge/scrooge/user/controller/MainPageController.java
@@ -2,7 +2,7 @@ package com.scrooge.scrooge.user.controller;
 
 import com.scrooge.scrooge.user.domain.User;
 import com.scrooge.scrooge.user.domain.UserOwningAvatar;
-import com.scrooge.scrooge.user.dto.MainPageDto;
+import com.scrooge.scrooge.user.dto.MainPageDTO;
 import com.scrooge.scrooge.user.repository.UserOwningAvatarRepository;
 import com.scrooge.scrooge.user.repository.UserRepository;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.math.BigInteger;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/main")
@@ -23,13 +24,13 @@ public class MainPageController {
         this.userOwningAvatarRepository = userOwningAvatarRepository;
     }
 
-    @GetMapping
-    public MainPageDto getMainPage(@PathVariable Long id) {
-        User user = userRepository.findById(id);
-                .orElseThrow(() -> new RuntimeException("User not found with id " + id));
+    @GetMapping("/{userId}")
+    public MainPageDTO getMainPage(@PathVariable Long userId) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new RuntimeException("User not found with id " + userId));
         UserOwningAvatar userOwningAvatar = userOwningAvatarRepository.findById(user.getId())
                 .orElseThrow(() -> new RuntimeException("UserOwningAvatar not found for user with id " + user.getId()));
 
-        return new MainPageDto(user.getNickname(), user.getLevelId(), user.getExp(), userOwningAvatar.getIsMainAvatar());
+        return new MainPageDTO(user.getNickname(), user.getLevel(), user.getExp(), user.getMainAvatar());
     }
 }
