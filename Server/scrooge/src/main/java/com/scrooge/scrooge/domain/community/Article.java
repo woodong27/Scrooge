2e7dfa9841f3daea647feac24cd4f004f421aef0
이하린 <gmlwls608@gmail.com>
2023-07-27 15:20:30 +0900

    Chore: Community Entity update #14

--- Server/scrooge/src/main/java/com/scrooge/scrooge/domain/community/Article.java
+++ Server/scrooge/src/main/java/com/scrooge/scrooge/domain/community/Article.java
@@ -3,8 +3,11 @@ package com.scrooge.scrooge.domain.community;
 import com.scrooge.scrooge.domain.User;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "article")
@@ -19,12 +22,23 @@ public class Article {
     @Column(length = 255, nullable = false)
     private String title;
 
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
 
+    @Column(length = 255, name = "img_address")
+    private String imgAdress; // 일단 nullable true하고 구현하기
+
+    @Column(name = "created_at")
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
 
     /* 연결 */
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
     private User user;
-
 
 }
--- Server/scrooge/src/main/java/com/scrooge/scrooge/domain/community/Comment.java
+++ Server/scrooge/src/main/java/com/scrooge/scrooge/domain/community/Comment.java
@@ -1,9 +1,13 @@
 package com.scrooge.scrooge.domain.community;
 
+import com.scrooge.scrooge.domain.User;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "comment")
@@ -15,7 +19,25 @@ public class Comment {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(length = 255, nullable = false)
+    private String content;
+
+    @Column(name = "created_at")
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
     /* 연결 */
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "article_id")
+    private Article article;
 
 
 }
