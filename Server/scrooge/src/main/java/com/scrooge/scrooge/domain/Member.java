003e0d8519a55f5796223226a8ceb130539dc19e
이하린 <gmlwls608@gmail.com>
2023-07-31 16:13:00 +0900

    Feat: 일일 정산 후 경험치 획득 API #22

--- Server/scrooge/src/main/java/com/scrooge/scrooge/domain/Member.java
+++ Server/scrooge/src/main/java/com/scrooge/scrooge/domain/Member.java
@@ -51,16 +51,16 @@ public class Member {
     @JoinColumn(name = "main_badge_id")
     private Badge mainBadge;
 
-    @Column
+    @Column(columnDefinition = "int default 0")
     private Integer exp;
 
-    @Column
+    @Column(columnDefinition = "int default 0")
     private Integer streak;
 
-    @Column(name = "weekly_goal")
+    @Column(name = "weekly_goal", columnDefinition = "int default 0")
     private Integer weeklyGoal;
 
-    @Column(name = "weekly_consum")
+    @Column(name = "weekly_consum", columnDefinition = "int default 0")
     private Integer weeklyConsum;
     // int -> Integer 로 변경하는게 좋을 것 같다.
 
