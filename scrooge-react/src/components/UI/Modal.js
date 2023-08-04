580f596747c51f2bcbf849fcd06f367cfebe44ec
강다은 <da010228@naver.com>
2023-08-04 16:06:56 +0900

    Refactor: 결제 내역 및 금액 수정 #27

--- scrooge-react/src/components/UI/Modal.js
+++ scrooge-react/src/components/UI/Modal.js
@@ -11,21 +11,30 @@ const Modal = ({
   onCloseModal,
 }) => {
   const [origin, setOrigin] = useState(amount);
-  const [price, setPrice] = useState(amount);
+  // const [price, setPrice] = useState(amount);
+  const [place, setPlace] = useState(usedAt);
+  const [category, setCategory] = useState(" ");
   const localContentInput = useRef();
+  const localPlaceInput = useRef();
 
-  const [category, setCategory] = useState();
   const handleCategory = (category) => {
     setCategory(category);
   };
 
   const handleEdit = () => {
+    if (place.length < 2) {
+      localPlaceInput.current.focus();
+      return;
+    }
     if (origin.length < 2) {
       localContentInput.current.focus();
       return;
     }
-    onEdit(id, origin, usedAt, cardName);
-    setPrice(origin);
+    if (category.length < 2) {
+      localContentInput.current.focus();
+      return;
+    }
+    onEdit(id, origin, place, cardName);
   };
 
   // 초기화 용도로 남겨두자..
@@ -40,10 +49,15 @@ const Modal = ({
         <div className={styles.modal}>
           <div className={styles.handle}> </div>
           <div className={styles.head}>
-            <h2>{usedAt}</h2>
+            <span>결제 내역 </span>
+            <input
+              ref={localPlaceInput}
+              value={place}
+              onChange={(e) => setPlace(e.target.value)}
+            />
           </div>
-          <div className={styles.amount}>
-            <span> ₩ </span>
+          <div className={styles.head}>
+            <span>결제 금액 </span>
             <input
               ref={localContentInput}
               value={origin}
@@ -55,52 +69,50 @@ const Modal = ({
             <div className={styles.content}>{cardName}</div>
           </div>
           <div className={styles.line}>
-            <div className={styles.title}>거래 일자</div>
+            <div className={styles.title}>결제 일자</div>
             <div className={styles.content}>
               {`${paidAt.slice(0, 19).split("T")[1]} `}
             </div>
           </div>{" "}
         </div>
+        <p className={styles.where}>어디에 썼나요?</p>
         <div className={styles.pickCategory}>
-          <h3>어디에 썼나요? {category}</h3>
-          <ul>
-            <button
-              className={styles.category}
-              onClick={() => handleCategory("식비")}
-            >
-              식비
-            </button>
-            <button
-              className={styles.category}
-              onClick={() => handleCategory("쇼핑")}
-            >
-              쇼핑
-            </button>
-            <button
-              className={styles.category}
-              onClick={() => handleCategory("문화생활")}
-            >
-              문화생활
-            </button>
-            <button
-              className={styles.category}
-              onClick={() => handleCategory("교통비")}
-            >
-              교통비
-            </button>
-            <button
-              className={styles.category}
-              onClick={() => handleCategory("정기구독")}
-            >
-              정기구독
-            </button>
-            <button
-              className={styles.category}
-              onClick={() => handleCategory("기타")}
-            >
-              기타
-            </button>
-          </ul>
+          <button
+            className={styles.category}
+            onClick={() => handleCategory("식비")}
+          >
+            식비
+          </button>
+          <button
+            className={styles.category}
+            onClick={() => handleCategory("쇼핑")}
+          >
+            쇼핑
+          </button>
+          <button
+            className={styles.category}
+            onClick={() => handleCategory("문화생활")}
+          >
+            문화생활
+          </button>
+          <button
+            className={styles.category}
+            onClick={() => handleCategory("교통비")}
+          >
+            교통비
+          </button>
+          <button
+            className={styles.category}
+            onClick={() => handleCategory("정기구독")}
+          >
+            정기구독
+          </button>
+          <button
+            className={styles.category}
+            onClick={() => handleCategory("기타")}
+          >
+            기타
+          </button>
         </div>
         <div className={styles.underBtn}>
           <button onClick={handleQuitEdit}>초기화</button>
