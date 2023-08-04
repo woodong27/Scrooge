5a0a2355bde24a54235d7b91364e11d0a9c69131
강다은 <da010228@naver.com>
2023-08-04 12:30:41 +0900

    Feat: 항목 별 아이템 선택  #27

--- scrooge-react/src/components/UI/Modal.js
+++ scrooge-react/src/components/UI/Modal.js
@@ -17,6 +17,11 @@ const Modal = ({
   const [price, setPrice] = useState(amount);
   const localContentInput = useRef();
 
+  const [category, setCategory] = useState();
+  const handleCategory = (category) => {
+    setCategory(category);
+  };
+
   const handleEdit = () => {
     if (origin.length < 2) {
       localContentInput.current.focus();
@@ -76,14 +81,44 @@ const Modal = ({
           </div>
         </div>
         <div className={styles.pickCategory}>
-          <h3>어디에 썼나요?</h3>
+          <h3>어디에 썼나요? {category}</h3>
           <ul>
-            <button className={styles.category}>식비</button>
-            <button className={styles.category}>쇼핑</button>
-            <button className={styles.category}>문화생활</button>
-            <button className={styles.category}>교통비</button>
-            <button className={styles.category}>정기구독</button>
-            <button className={styles.category}>기타</button>
+            <button
+              className={styles.category}
+              onClick={() => handleCategory("식비")}
+            >
+              식비
+            </button>
+            <button
+              className={styles.category}
+              onClick={() => handleCategory("쇼핑")}
+            >
+              쇼핑
+            </button>
+            <button
+              className={styles.category}
+              onClick={() => handleCategory("문화생활")}
+            >
+              문화생활
+            </button>
+            <button
+              className={styles.category}
+              onClick={() => handleCategory("교통비")}
+            >
+              교통비
+            </button>
+            <button
+              className={styles.category}
+              onClick={() => handleCategory("정기구독")}
+            >
+              정기구독
+            </button>
+            <button
+              className={styles.category}
+              onClick={() => handleCategory("기타")}
+            >
+              기타
+            </button>
           </ul>
         </div>
       </div>
