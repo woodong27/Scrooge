b55825a810fd9ae7d03181622b91f878edbe52b9
박진희 <wlsl0620@naver.com>
2023-08-04 12:41:10 +0900

    Init: 설정 페이지 #26

--- scrooge-react/src/pages/MyPage/MyPageHeader.js
+++ scrooge-react/src/pages/MyPage/MyPageHeader.js
@@ -1,10 +1,16 @@
 import styles from "./MyPageHeader.module.css";
+import { Link } from "react-router-dom";
 
 const MyPageHeader = () => {
   return(
     <div className={styles["header-container"]}>
       <div className={styles["header-text"]}>마이 페이지</div>
-      <img  className={styles["header-icon"]} src={`${process.env.PUBLIC_URL}/images/setting-icon.png`} alt="설정 아이콘"/>
+      <Link to="/settings">
+        <img 
+          className={styles["header-icon"]} 
+          src={`${process.env.PUBLIC_URL}/images/setting-icon.png`} 
+          alt="설정 아이콘"/>
+      </Link>
     </div>
   );
 };
