22855ba1068095de6552e98063cdb2d78ef4c9b3
최동우 <fdf5548@naver.com>
2023-08-07 15:41:47 +0900

    Init : Upload files about community api

---
+++ Server/scrooge/src/main/java/com/scrooge/scrooge/dto/communityDto/ArticleGoodCountDto.java
@@ -0,0 +1,12 @@
+package com.scrooge.scrooge.dto.communityDto;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class ArticleGoodCountDto {
+
+    private Integer goodCount;
+}
