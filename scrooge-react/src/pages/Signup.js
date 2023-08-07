542f096363b6585aca31ea71f3277f6eaddd9a1d
강다은 <da010228@naver.com>
2023-08-07 12:26:37 +0900

    Chore: Signup 검사 메시지 및 스크롤 #38

--- scrooge-react/src/pages/Signup.js
+++ scrooge-react/src/pages/Signup.js
@@ -33,19 +33,39 @@ const Signup = ({ loginHandler }) => {
   const handleSignup = () => {
     if (state.nickname.length < 4) {
       nicknameInput.current.focus();
+      const errorDiv = document.getElementById("nickname");
+      errorDiv.style.display = "block";
       return;
+    } else {
+      const errorDiv = document.getElementById("nickname");
+      errorDiv.style.display = "none";
     }
     if (state.email.length < 10) {
       emailInput.current.focus();
+      const errorDiv = document.getElementById("email");
+      errorDiv.style.display = "block";
       return;
+    } else {
+      const errorDiv = document.getElementById("email");
+      errorDiv.style.display = "none";
     }
     if (state.password.length < 8) {
       passwordInput.current.focus();
+      const errorDiv = document.getElementById("password");
+      errorDiv.style.display = "block";
       return;
+    } else {
+      const errorDiv = document.getElementById("password");
+      errorDiv.style.display = "none";
     }
     if (state.password !== state.passwordcheck) {
       passwordcheckInput.current.focus();
+      const errorDiv = document.getElementById("passwordcheck");
+      errorDiv.style.display = "block";
       return;
+    } else {
+      const errorDiv = document.getElementById("passwordcheck");
+      errorDiv.style.display = "none";
     }
 
     const obj = {
@@ -74,40 +94,58 @@ const Signup = ({ loginHandler }) => {
       <div className={styles.empty} />
       <CharacterCard>
         <div className={styles.title}>회원가입</div>
-        <input
-          className={styles.input}
-          ref={nicknameInput}
-          name="nickname"
-          value={state.nickname}
-          placeholder="닉네임을 입력해주세요"
-          onChange={handleChangeState}
-        />
-        <input
-          className={styles.input}
-          ref={emailInput}
-          name="email"
-          value={state.email}
-          placeholder="이메일을 입력해주세요"
-          onChange={handleChangeState}
-        />
-        <input
-          className={styles.input}
-          ref={passwordInput}
-          name="password"
-          type="password"
-          value={state.password}
-          placeholder="비밀번호를 입력해주세요"
-          onChange={handleChangeState}
-        />
-        <input
-          className={styles.input}
-          ref={passwordcheckInput}
-          name="passwordcheck"
-          type="password"
-          value={state.passwordcheck}
-          placeholder="비밀번호를 확인해주세요"
-          onChange={handleChangeState}
-        />
+
+        <div className={styles.frame}>
+          <input
+            className={styles.input}
+            ref={nicknameInput}
+            name="nickname"
+            value={state.nickname}
+            placeholder="닉네임을 입력해주세요"
+            onChange={handleChangeState}
+          />
+          <div id="nickname" className={styles.error}>
+            닉네임을 4글자 이상 입력해주세요.
+          </div>
+
+          <input
+            className={styles.input}
+            ref={emailInput}
+            name="email"
+            value={state.email}
+            placeholder="이메일을 입력해주세요"
+            onChange={handleChangeState}
+          />
+          <div id="email" className={styles.error}>
+            올바른 이메일을 입력해주세요.
+          </div>
+
+          <input
+            className={styles.input}
+            ref={passwordInput}
+            name="password"
+            type="password"
+            value={state.password}
+            placeholder="비밀번호를 입력해주세요"
+            onChange={handleChangeState}
+          />
+          <div id="password" className={styles.error}>
+            비밀번호를 8글자 이상 입력해주세요.
+          </div>
+
+          <input
+            className={styles.input}
+            ref={passwordcheckInput}
+            name="passwordcheck"
+            type="password"
+            value={state.passwordcheck}
+            placeholder="비밀번호를 확인해주세요"
+            onChange={handleChangeState}
+          />
+          <div id="passwordcheck" className={styles.error}>
+            비밀번호를 다시 확인해주세요.
+          </div>
+        </div>
       </CharacterCard>
 
       <ButtonWhite text="회원가입" onClick={handleSignup}></ButtonWhite>
