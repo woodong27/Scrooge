2366e1c7db423ee029824cc475bb56e93cc7084a
강다은 <da010228@naver.com>
2023-08-08 22:37:08 +0900

    Feat: 댓글 삭제  API 수정 필요 #40

--- scrooge-react/src/pages/Community/Comment.js
+++ scrooge-react/src/pages/Community/Comment.js
@@ -1,11 +1,25 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { useSelector } from "react-redux";
 import styles from "./Comment.module.css";
 
 const Comment = (props) => {
   const globalToken = useSelector((state) => state.globalToken);
 
-  useEffect(() => {});
+  const [isEdit, setIsEdit] = useState(false);
+  const [comment, setComment] = useState("");
+
+  useEffect(() => {
+    setComment(props.content);
+  }, []);
+
+  const handleOpen = () => {
+    setIsEdit(true);
+  };
+
+  const handleDelete = () => {
+    console.log(props);
+    props.commentDelete(props.id);
+  };
 
   return (
     <div className={styles.box}>
@@ -18,12 +32,27 @@ const Comment = (props) => {
         <div className={styles.nickname}>{props.memberNickname}</div>
         <div className={styles.createdAt}>
           {props.createdAt.split("T")[0]}{" "}
-          {props.createdAt.includes(".")
-            ? props.createdAt.split("T")[1].split(".")[0]
-            : props.createdAt.split("T")[1]}
+          {props.createdAt.split("T")[1].slice(0, 5)}
         </div>
       </div>
-      <div className={styles.content}>{props.content}</div>
+      <div className={styles.content}>
+        <div>
+          {comment}
+          {isEdit ? (
+            <div className={styles.delete} onClick={handleDelete}>
+              {" "}
+              삭제{" "}
+            </div>
+          ) : (
+            <div className={styles.option} onClick={handleOpen}>
+              <img
+                src={`${process.env.PUBLIC_URL}/images/option.png`}
+                alt="더보기"
+              />
+            </div>
+          )}
+        </div>
+      </div>
     </div>
   );
 };
