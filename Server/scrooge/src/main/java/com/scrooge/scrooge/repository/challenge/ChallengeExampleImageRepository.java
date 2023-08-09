f20cc0fc7e9ebae55151628bee26eaa54a7461ba
최동우 <fdf5548@naver.com>
2023-08-09 15:33:59 +0900

    Feat : Update files about challenge api

--- Server/scrooge/src/main/java/com/scrooge/scrooge/repository/challenge/ChallengeExampleImageRepository.java
+++ Server/scrooge/src/main/java/com/scrooge/scrooge/repository/challenge/ChallengeExampleImageRepository.java
@@ -7,6 +7,6 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface ChallengeExampleImageRepository extends JpaRepository<ChallengeExampleImage, Long> {
-    @Query("SELECT ce FROM ChallengeExampleImage ce WHERE ce.challenge.id = :challengeId")
+
     List<ChallengeExampleImage> findByChallengeId(Long challengeId);
 }
