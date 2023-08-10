3117b91ae468f47858a00cf05e692764b990a164
박진희 <wlsl0620@naver.com>
2023-08-10 10:08:28 +0900

    Chore: weekly report 수정중 #26

--- scrooge-react/src/pages/MyPage/ReportWeek.js
+++ scrooge-react/src/pages/MyPage/ReportWeek.js
@@ -1,80 +1,86 @@
 import React, { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
-import { BarChart, Bar, XAxis} from 'recharts';
+import { BarChart, Bar, XAxis } from 'recharts';
 import styles from "./ReportWeek.module.css";
 
 const ReportWeek = () => {
   const globalToken = useSelector((state) => state.globalToken);
+  const [data, setData] = useState([]);
+  const [startPoint, setStartPoint] = useState(null);  // 시작일은 월요일 
+  const [weeklyDates, setWeeklyDates] = useState([]); // 주간 날짜들 저장 
 
-  const [startDate, setStartDate] = useState(new Date());
-  const [dailyTot, setDailyTot] = useState([]);
-  
-  useEffect(() => {
-    dailyData(`${startDate.getFullYear()}-${(startDate.getMonth()+1).toString().padStart(2,"0")}-${startDate.getDate().toString().padStart(2,"0")}`);
-  },[]);
+  // api 받아오기 위해 데이터 변형 
+  const formatDate = (date) => {
+    const year = date.getFullYear();
+    const month = (date.getMonth() +1).toString().padStart(2,"0");
+    const day = date.getDate().toString().padStart(2,"0");
+    return `${year}-${month}-${day}`;
+  };
 
-  // useEffect(()=>{
-  //   dailyData("2023-08-07")
-  // },[])
+  const getStandardDate = () => {
+    const currentDate = new Date();         // 현재 날짜
+    const dayOfWeek = currentDate.getDay(); // 현재 요일
+    const daysToAdd = dayOfWeek === 0 ? -6 : 1 - dayOfWeek // 시작요일: 월요일 
+    const startDate = new Date(currentDate);
+    startDate.setDate(currentDate.getDate() + daysToAdd);
 
-  const dailyData = (days) => {
+    const endDate = new Date(startDate);
+    endDate.setDate(startDate.getDate() + 6);
+
+    return { startDate, endDate };
+  }
+
+
+  const getWeeklyDatesArray = () => {
+    const {startDate, endDate } = getStandardDate();
+    const weeklyDatesArray = [];
+
+    for (let date = new Date(startDate); date <= endDate ; date.setDate(date.getDate() + 1)) {
+      weeklyDatesArray.push(new Date(date));
+    }
+
+    return weeklyDatesArray;
+  }
+
+  const getWeeklyData = async () => {
+    const { startDate, endDate } = getStandardDate();
+    const weeklyDatesArray = getWeeklyDatesArray(startDate);
+
     const postData = {
       method: "GET",
-      headers:{
+      headers: {
         Authorization: globalToken,
       },
     };
 
-    fetch(`https://day6scrooge.duckdns.org/api/payment-history/date/${days}`,
-    postData
-    )
-      .then((resp) => resp.json())
-      .then((data) => {
-        console.log("원래rj", data)
-        setDailyTot(data.map(item => item.amount));
-        // console.log(dailyTot);
-      })
-      .catch((error) => console.log(error));
+    try{
+      const weeklyDataPromises = weeklyDatesArray.map(async (date) => {
+        const formattedDate = formatDate(date);
+        const resp = await fetch(`https://day6scrooge.duckdns.org/api/payment-history/date-total/${formattedDate}`, postData)
+        const data = await resp.json();
+        return {date: formattedDate, amount: data};
+      });
+
+      const weeklyDataResults = await Promise.all(weeklyDataPromises)
+      setData(weeklyDataResults.map((result) => result.amount));
+      setStartPoint(startDate);
+      setWeeklyDates(weeklyDataResults.map((result)=> result.date));
+
+      console.log("일주일 데이터", weeklyDataResults.map((result) => result.amount));
+      console.log("일주일 날짜", weeklyDataResults.map((result) => result.date));
+
+    } catch (error) {
+      console.log(error);
+    }
   };
+
+
 
   useEffect(() => {
-    console.log("업뎃rj", dailyTot.reduce((acc, amount) => acc + amount, 0))
-  },[dailyTot])
+    getWeeklyData();
+  },[]) 
 
 
-
-  const handleLastWeek = () => {
-    const lastWeek = new Date(startDate);
-    lastWeek.setDate(startDate.getDate() - 7);
-    setStartDate(lastWeek);
-  };
-  const handleNextWeek = () => {
-    const nextWeek = new Date(startDate);
-    nextWeek.setDate(startDate.getDate() + 7);
-    setStartDate(nextWeek);
-  }
-
-  const data = [];
-  const dayOfWeek = startDate.getDay(); 
-  const startPoint = new Date(startDate);
-  startPoint.setDate(startDate.getDate() - dayOfWeek + (dayOfWeek === 0? -6 : 1))
-  const endPoint = new Date(startPoint) ;
-  endPoint.setDate(startPoint.getDate() + 6);    
-  
-  for (let i = 0; i<7; i++) {
-    const labelDate = new Date(startPoint);
-    labelDate.setDate(startPoint.getDate() + i);
-    const month = (labelDate.getMonth() + 1).toString().padStart(1, "0");
-    const day = labelDate.getDate().toString().padStart(1,"0");
-    data.push({
-      name: `${month}/${day}`,
-      amount: 5000,
-    });
-  }
-
-  const avgAmt = data.reduce((acc, cur) => acc + cur.amount, 0) / data.length;
-  const minAmt = data.reduce((min, cur) => (cur.amount < min ? cur.amount : min ), data[0].amount);
-  
   return (
     <div>
       {/* 날짜 이동 */}
@@ -82,46 +88,47 @@ const ReportWeek = () => {
         <button onClick={handleLastWeek}>
           <img
             src={`${process.env.PUBLIC_URL}/images/left.svg`}
-            alt="이전"/>
+            alt="이전" />
         </button>
-        <h3>{`${startPoint.getMonth() + 1}월 ${startPoint.getDate()}일 ~ ${endPoint.getMonth() + 1}월 ${endPoint.getDate()}일`}</h3>
-        <button onClick={handleNextWeek}>
+        <h3>M월 D일 ~ M월 D일</h3>
+        <button>
           <img
-              src={`${process.env.PUBLIC_URL}/images/right.svg`}
-              alt="다음"
-          />          
+            src={`${process.env.PUBLIC_URL}/images/right.svg`}
+            alt="다음"
+          />
         </button>
       </div>
-
+  
       {/* 소비 요약 */}
       <div className={styles.reportContent}>
         <div className={styles.weekAvg}>
           <div>평균소비금액</div>
-          <div><b>{Math.floor(avgAmt).toLocaleString()}</b> 원</div>
+          <div><b>얼마얼마</b> 원</div>
         </div>
         <div className={styles.weekMax}>
           <div>최대절약금액</div>
-          <div><b>{minAmt.toLocaleString()}</b> 원</div>
+          <div><b>얼마얼마~</b> 원</div>
         </div>
       </div>
-
+  
       {/* 소비 그래프 */}
       <div className={styles.chartContainer}>
         <BarChart width={350} height={200} data={data}>
           <Bar dataKey="amount" fill="#A2D660" />
-          <XAxis dataKey="name" />
+          <XAxis dataKey="date" interval={0} tick={{ angle: -45 }} />
         </BarChart>
       </div>
-
+  
       {/* 카테고리 */}
       <div className={styles.cardContainer}>
         <div>카테고리 카드1</div>
         <div>카테고리 카드2</div>
-      </div>      
-
+      </div>
+  
     </div>
   );
 }
+
 
 
 export default ReportWeek;
