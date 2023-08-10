bb07060e441e29190a76bec410d61f686126197e
이하린 <gmlwls608@gmail.com>
2023-08-10 11:51:56 +0900

    Comment: 안 쓰는 import 구문 지우기 #51

--- Server/scrooge/src/main/java/com/scrooge/scrooge/domain/challenge/ChallengeChattingMessage.java
+++ Server/scrooge/src/main/java/com/scrooge/scrooge/domain/challenge/ChallengeChattingMessage.java
@@ -3,8 +3,6 @@ package com.scrooge.scrooge.domain.challenge;
 import com.scrooge.scrooge.domain.member.Member;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.Fetch;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
