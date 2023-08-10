17b02dd99ebb44f6ff0a6f8f63ea04508a52dc3d
이하린 <gmlwls608@gmail.com>
2023-08-10 11:29:00 +0900

    Init: Challenge Chatting 구현 위한 Entity 생성 #51

---
+++ Server/scrooge/src/main/java/com/scrooge/scrooge/domain/challenge/ChallengeChattingMessage.java
@@ -0,0 +1,37 @@
+package com.scrooge.scrooge.domain.challenge;
+
+import com.scrooge.scrooge.domain.member.Member;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.Fetch;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "challenge_chatting_message")
+@Data
+@NoArgsConstructor
+public class ChallengeChattingMessage {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    @Column(name = "created_at", columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
+    private LocalDateTime createdAt;
+
+    /* 연결 */
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "challenge_chatting_room_id")
+    private ChallengeChattingRoom challengeChattingRoom;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "message_author")
+    private Member member;
+
+}
---
+++ Server/scrooge/src/main/java/com/scrooge/scrooge/domain/challenge/ChallengeChattingRoom.java
@@ -0,0 +1,37 @@
+package com.scrooge.scrooge.domain.challenge;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.scrooge.scrooge.domain.member.Member;
+
+@Entity
+@Table(name = "challenge_chatting_room")
+@Data
+@NoArgsConstructor
+public class ChallengeChattingRoom {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    /* TEAM */
+    @Column
+    Integer team;
+
+    /* 연결 */
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "room_member")
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "challenge_id")
+    private Challenge challenge;
+
+    @OneToMany(mappedBy = "challenge_chatting_room", cascade = CascadeType.REMOVE, fetch = FetchType.LAZY)
+    private List<ChallengeChattingMessage> challengeChattingMessageList = new ArrayList<>();
+}
