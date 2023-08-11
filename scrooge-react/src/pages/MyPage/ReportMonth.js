147a6498375b2a487573e2f2f1a87101e12761df
박진희 <wlsl0620@naver.com>
2023-08-11 15:07:35 +0900

    Feat: Calendar 생성 #26

--- scrooge-react/src/pages/MyPage/ReportMonth.js
+++ scrooge-react/src/pages/MyPage/ReportMonth.js
@@ -1,46 +1,119 @@
-import { useState } from "react";
-import styles from "./ReportWeek.module.css";
+import { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
+import styles from "./ReportMonth.module.css";
+
+
+const titles = ['월','화','수','목','금','토','일'];
 
 const ReportMonth = () => {
+  const globalToken = useSelector((state) => state.globalToken);
+
   const currentDate = new Date();
-  const currentMonth = currentDate.getMonth() + 1;
-  const currentYear = currentDate.getFullYear();
-  const [selectedMonth, setSelectedMonth] = useState(currentMonth);
-  const [selectedYear, setSelectedYear] = useState(currentYear);
+  const year = currentDate.getFullYear();
+  const month = currentDate.getMonth() +1;
 
-  const handleMonth = (change) => {
-    let newMonth = selectedMonth + change;
-    let newYear = selectedYear;
+  const formattedMonth = `${year}-${String(month).padStart(2,'0')}`;
+  const [monthlyData, setMonthlyData] = useState([]);
 
-    if (newMonth < 1) {
-      newMonth = 12;
-      newYear--;
-    } else if (newMonth > 12) {
-      newMonth = 1;
-      newYear++;
-    }
+  useEffect(() => {
+    const getMonthlyData = async () => {
+      const postData = {
+        mehtod: "GET",
+        headers: {
+          Authorization: globalToken,
+        },
+      };
 
-    setSelectedMonth(newMonth);
-    setSelectedYear(newYear);
-  }
+      try{
+        const resp = await fetch(`https://day6scrooge.duckdns.org/api/payment-history/month/${formattedMonth}`,postData)
+        const data = await resp.json();
+        setMonthlyData(data);
+        console.log("데이터..뭡네까", )
+      } catch (error) {
+        console.log(error)
+      }
+    };
+    getMonthlyData();
+  },[formattedMonth]);
+
+
+
+  const getMonthInfo = (year, month) => {
+    const startDay = new Date(year, month -1, 1).getDay();
+    const endDate = new Date(year, month, 0).getDate();
+
+    const adjStartDay = startDay === 0 ? 6 : startDay -1;
+    const info = {startDay: adjStartDay, endDate}
+    console.log(info)
+    // return {startDay: adjStartDay, endDate};
+    return info
+  };
+
+  const getCalendar = (year, month) => {
+    const { endDate, startDay } = getMonthInfo(year, month);
+    const weekNumber = Math.ceil((startDay + endDate) / 7);
+    const calendar = [];
+
+    let nowDate = 0;
+    let nowDay = 0;
+
+    for (let i = 0; i <weekNumber ; i++) {
+      const nowWeek = [];
+      for (let j = 0; j < 7; j++) {
+        if (startDay <= nowDay && nowDate < endDate) {
+          nowDate++;
+          nowWeek.push(nowDate);
+        } else {
+          nowWeek.push(0);
+        };
+        nowDay++;
+      }
+      calendar.push(nowWeek);
+    };
+    return calendar;
+  };
+
+
+
+  const datas = getCalendar(year, month);
+  const components = [];
+
+  datas.forEach((week, weekIndex) => {
+    const weeks = week.map((date, dayIndex) => ({
+      key: date !==0 ? date: dayIndex,
+      render: date !==0 ? <span>{date}</span> : <span />,
+    }));
+
+    const weekData = {
+      id: `w_${8}${weekIndex}`,
+      columns: weeks,
+    };
+    components.push(weekData);
+  });
 
   return(
-    <div className={styles.reportContainer}>
-      {/* 날짜 */}
-      <div className={styles.weekDays}>
-        <button onClick={() => handleMonth(-1)}>
-          <img
-            src={`${process.env.PUBLIC_URL}/images/left.svg`}
-            alt="이전"
-          />
-        </button>
-        <h3>{`${selectedYear}년 ${selectedMonth}월`}</h3>
-        <button onClick={() => handleMonth(1)}>
-          <img
-            src={`${process.env.PUBLIC_URL}/images/right.svg`}
-            alt="다음"
-          />          
-        </button>
+    <div>
+      <h2>{year}년 {month}월</h2>
+      <div className={styles.tableContainer}>
+        <table className={styles.tableContent}>
+          <thead>
+            <tr>
+              {titles.map((title, index) => (
+                <th key={index}>{title}</th>
+              ))}
+            </tr>
+          </thead>
+          <tbody>
+            {components.map((weekData, weekIndex) => (
+              <tr key={weekData.id}>
+                {weekData.columns.map(column => (
+                  <td key={column.key}>{column.render}</td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+
       </div>
     </div>
   )
