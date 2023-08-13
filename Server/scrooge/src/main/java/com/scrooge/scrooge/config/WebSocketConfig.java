f71cc26314cb1d7e9f16f348c13a18a854cbc113
stubborngastropod <dnchudnjee@gmail.com>
2023-08-13 15:12:45 +0900

    Chore: modify for websocket api

--- Server/scrooge/src/main/java/com/scrooge/scrooge/config/WebSocketConfig.java
+++ Server/scrooge/src/main/java/com/scrooge/scrooge/config/WebSocketConfig.java
@@ -1,34 +1,23 @@
 package com.scrooge.scrooge.config;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
-import org.springframework.messaging.simp.config.AbstractMessageBrokerConfiguration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.config.annotation.*;
+import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 
 @Configuration
 @EnableWebSocketMessageBroker
-public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
-
-    private final WebSocketHandler webSocketHandler;
-
-    @Autowired
-    public WebSocketConfig(WebSocketHandler webSocketHandler) {
-        this.webSocketHandler = webSocketHandler;
-    }
+public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
 
     @Override
-    public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.enableSimpleBroker("/chatroom", "/user");
-        registry.setApplicationDestinationPrefixes("/app");
-        registry.setUserDestinationPrefix("/user");
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.enableSimpleBroker("/topic"); // 메시지 브로커가 구독 대상 메시지 전송
+        config.setApplicationDestinationPrefixes("/app"); // 클라이언트에서 메시지 발행 시 필요한 접두사
     }
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws").setAllowedOriginPatterns("*").withSockJS();
+        registry.addEndpoint("/ws"); // 클라이언트 연결을 위한 엔드포인트 등록
     }
 }
