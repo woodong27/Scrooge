831499e94638337d6c263c0786dc28cafe9e110f
강다은 <da010228@naver.com>
2023-08-15 01:26:33 +0900

    Feat: 메인 캐릭터 움찔 #27

--- scrooge-react/src/pages/Main/Main.js
+++ scrooge-react/src/pages/Main/Main.js
@@ -23,6 +23,12 @@ const Main = (props) => {
   const [weeklyGoal, setWeeklyGoal] = useState();
   const [weeklyConsum, setWeeklyConsum] = useState();
 
+  const [images, setImages] = useState([
+    "https://storage.googleapis.com/scroogestorage/avatars/2-1.png",
+    "https://storage.googleapis.com/scroogestorage/avatars/2-2.png",
+  ]);
+  const [imageIndex, setImageIndex] = useState(0);
+
   const handleOpen = () => {
     setIsEdit(true);
   };
@@ -83,34 +89,52 @@ const Main = (props) => {
         setMessage(data.message);
         setWeeklyGoal(data.weeklyGoal);
         setWeeklyConsum(data.weeklyConsum);
+        setImages([
+          `https://storage.googleapis.com/scroogestorage/avatars/${data.mainAvatar.id}-1.png`,
+          `https://storage.googleapis.com/scroogestorage/avatars/${data.mainAvatar.id}-2.png`,
+        ]);
       })
       .catch((error) => console.log(error));
 
     const formattedDate = `2023-${month.toString().padStart(2, "0")}-${day
       .toString()
       .padStart(2, "0")}`;
-
     getTotal(formattedDate);
-    const todayData = {
-      method: "GET",
-      headers: {
-        Authorization: globalToken,
-      },
-    };
 
-    fetch(
-      `https://day6scrooge.duckdns.org/api/payment-history/date/${formattedDate}`,
-      todayData
-    )
-      .then((resp) => resp.json())
-      .then((data) => {
-        // const index = data.findIndex((item) => !item.isSettled);
-        // if (index === -1) {
-        //   setSettlement(true);
-        // }
-      })
-      .catch((error) => console.log(error));
+    //안 쓰는 것 같기도 해서 일단 주석처리
+    // const todayData = {
+    //   method: "GET",
+    //   headers: {
+    //     Authorization: globalToken,
+    //   },
+    // };
+
+    // fetch(
+    //   `https://day6scrooge.duckdns.org/api/payment-history/date/${formattedDate}`,
+    //   todayData
+    // )
+    //   .then((resp) => resp.json())
+    //   .then((data) => {
+    //     // const index = data.findIndex((item) => !item.isSettled);
+    //     // if (index === -1) {
+    //     //   setSettlement(true);
+    //     // }
+    //   })
+    //   .catch((error) => console.log(error));
   }, [isConsum]); //이걸 onCreate되면 하는 걸로 해야겠는데
+
+  //이미지 움직이게
+
+  const changeImage = () => {
+    setImageIndex((prevIndex) => (prevIndex + 1) % images.length);
+  };
+  useEffect(() => {
+    const imageInterval = setInterval(changeImage, 1000); // 1초마다 이미지 변경
+
+    return () => {
+      clearInterval(imageInterval);
+    };
+  }, []);
 
   //주간 목표 설정
   const setGoal = (goal) => {
@@ -177,22 +201,43 @@ const Main = (props) => {
               </div>
               <div className={styles.border} />
 
-              <span className={styles.charactercoin}>
-                <img
-                  className={styles.character}
-                  src={`https://storage.googleapis.com/scroogestorage/avatars/${data.mainAvatar.id}-2.png`}
-                  alt="캐릭터"
-                />
-
-                <div className={styles.coin} onClick={consumTrueHandler}>
+              {settlement ? (
+                <span className={styles.charactercoin}>
                   <img
-                    src={`${process.env.PUBLIC_URL}/images/coin.png`}
-                    alt="코인"
+                    className={styles.character}
+                    src={`https://storage.googleapis.com/scroogestorage/avatars/${data.mainAvatar.id}-1.png`}
+                    alt="캐릭터"
                   />
-                  <div className={styles.payBtn}>정산하기</div>{" "}
-                  <div className={styles.streak}>{data.streak}일 째</div>
-                </div>
-              </span>
+
+                  <div className={styles.coin} onClick={consumTrueHandler}>
+                    <img
+                      src={`${process.env.PUBLIC_URL}/images/coin.png`}
+                      alt="코인"
+                    />
+                    <div className={styles.payBtn}>정산하기</div>{" "}
+                    <div className={styles.streak}>{data.streak}일 째</div>
+                  </div>
+                </span>
+              ) : (
+                <span className={styles.charactercoin}>
+                  <img
+                    className={styles.character}
+                    src={images[imageIndex]}
+                    alt="캐릭터"
+                  />
+
+                  {imageIndex === 1 && (
+                    <div className={styles.coin} onClick={consumTrueHandler}>
+                      <img
+                        src={`${process.env.PUBLIC_URL}/images/coin.png`}
+                        alt="코인"
+                      />
+                      <div className={styles.payBtn}>정산하기</div>{" "}
+                      <div className={styles.streak}>{data.streak}일 째</div>
+                    </div>
+                  )}
+                </span>
+              )}
 
               <div className={styles.statemessage}>
                 {isEdit ? (
