fc170ceb176736b2f31ace4b81767b88f09b6f0e
이하린 <gmlwls608@gmail.com>
2023-08-14 17:39:56 +0900

    Chore: 오류 해결 위한 코드 수정

--- scrooge-react/src/pages/Main/Main.js
+++ scrooge-react/src/pages/Main/Main.js
@@ -75,7 +75,7 @@ const Main = (props) => {
         Authorization: globalToken,
       },
     };
-    fetch("http://localhost:8081/api/member/info", postData)
+    fetch("https://day6scrooge.duckdns.org/api/member/info", postData)
       .then((resp) => resp.json())
       .then((data) => {
         console.log(data);
