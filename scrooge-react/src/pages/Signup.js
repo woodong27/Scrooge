fb0a6eda7c1c051ef1de39d94340be6576c65940
강다은 <da010228@naver.com>
2023-08-17 01:28:00 +0900

    Refactor: UI 통합 및 삭제 #64

--- scrooge-react/src/pages/Signup.js
+++ scrooge-react/src/pages/Signup.js
@@ -2,7 +2,6 @@ import React, { useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 import BackGround from "../components/BackGround";
-import ButtonWhite from "../components/Button/ButtonWhite";
 import Card from "../components/UI/Card";
 import styles from "./Signup.module.css";
 
