a88e6cfcde5b2f9bb5b9a03c37efc644b631286b
강다은 <da010228@naver.com>
2023-08-17 19:45:56 +0900

    Refactor: Footer 아래 작은 여백 없애기 (임시) #64

--- scrooge-react/src/components/Footer.js
+++ scrooge-react/src/components/Footer.js
@@ -4,53 +4,55 @@ import styles from "./Footer.module.css";
 
 const Footer = () => {
   return (
-    <ul className={styles.list}>
-      <li>
-        <Link to="/">
-          <img
-            src={`${process.env.PUBLIC_URL}/images/nav-icons/homeNew.png`}
-            alt="홈"
-          />
-          <div>홈</div>
-        </Link>
-      </li>
-      <li>
-        <Link to="/community">
-          <img
-            src={`${process.env.PUBLIC_URL}/images/nav-icons/peoplesNew.png`}
-            alt="커뮤니티"
-          />
-          <div>빌리지</div>
-        </Link>
-      </li>
-      <li>
-        <Link to="/challenge">
-          <img
-            src={`${process.env.PUBLIC_URL}/images/nav-icons/challengeNew.png`}
-            alt="챌린지"
-          />
-          <div>파이트</div>
-        </Link>
-      </li>
-      <li>
-        <Link to="/quest">
-          <img
-            src={`${process.env.PUBLIC_URL}/images/nav-icons/questNew.png`}
-            alt="퀘스트"
-          />
-          <div>드림</div>
-        </Link>
-      </li>
-      <li>
-        <Link to="/mypage">
-          <img
-            src={`${process.env.PUBLIC_URL}/images/nav-icons/profileNew.png`}
-            alt="프로필"
-          />
-          <div>프로필</div>
-        </Link>
-      </li>
-    </ul>
+    <div className={styles.body}>
+      <ul className={styles.list}>
+        <li>
+          <Link to="/">
+            <img
+              src={`${process.env.PUBLIC_URL}/images/nav-icons/homeNew.png`}
+              alt="홈"
+            />
+            <div>홈</div>
+          </Link>
+        </li>
+        <li>
+          <Link to="/community">
+            <img
+              src={`${process.env.PUBLIC_URL}/images/nav-icons/peoplesNew.png`}
+              alt="커뮤니티"
+            />
+            <div>빌리지</div>
+          </Link>
+        </li>
+        <li>
+          <Link to="/challenge">
+            <img
+              src={`${process.env.PUBLIC_URL}/images/nav-icons/challengeNew.png`}
+              alt="챌린지"
+            />
+            <div>파이트</div>
+          </Link>
+        </li>
+        <li>
+          <Link to="/quest">
+            <img
+              src={`${process.env.PUBLIC_URL}/images/nav-icons/questNew.png`}
+              alt="퀘스트"
+            />
+            <div>드림</div>
+          </Link>
+        </li>
+        <li>
+          <Link to="/mypage">
+            <img
+              src={`${process.env.PUBLIC_URL}/images/nav-icons/profileNew.png`}
+              alt="프로필"
+            />
+            <div>프로필</div>
+          </Link>
+        </li>
+      </ul>
+    </div>
   );
 };
 
