0419ddbe002e01117e83459aed30753777918ec0
이동주 <leednj0113@naver.com>
2023-08-17 21:28:38 +0900

    Revert "Style: 챌린지 상세보기(S8+)"
    This reverts commit 1a19b67d627c35f89ce1a7d8224dffeb9699d2eb.

--- scrooge-react/src/pages/Challenge/ChallengeJoin.js
+++ scrooge-react/src/pages/Challenge/ChallengeJoin.js
@@ -78,79 +78,77 @@ const ChallengeJoin = () => {
 
   return (
     <div className={styles.layout}>
-      <div className={styles.body}>
-        <Link className={styles.back} to="/challenge">
-          <img src={backImg} alt="뒤로가기"></img>
-        </Link>
-        <img className={styles.img} src={data.mainImageAddress} alt="더미" />
-        <div className={styles.container}>
-          <h1>{data.title}</h1>
-          <p className={styles.tag}>
-            #{data.category} #{data.period}
-          </p>
-          <div className={styles.content}>
-            <span>
-              현재인원: {data.currentParticipants} / {data.minParticipants}명
-            </span>
-            <span>경험치 500+</span>
-          </div>
+      <Link className={styles.back} to="/challenge">
+        <img src={backImg} alt="뒤로가기"></img>
+      </Link>
+      <img className={styles.img} src={data.mainImageAddress} alt="더미" />
+      <div className={styles.container}>
+        <h1>{data.title}</h1>
+        <p className={styles.tag}>
+          #{data.category} #{data.period}
+        </p>
+        <div className={styles.content}>
+          <span>
+            현재인원: {data.currentParticipants} / {data.minParticipants}명
+          </span>
+          <span>경험치 500+</span>
         </div>
-        <div className={styles.mid_container}>
-          <h2>이렇게 인증해주세요</h2>
-          <div>- 매일 전날의 소비 내역을 불러와주세요.</div>
-          <div>- 사진이 잘 보이도록 촬영해주세요.</div>
-          <div className={styles.auth_img}>
-            {data.challengeExampleImageDtoList &&
-              data.challengeExampleImageDtoList.map((e, i) => (
-                <img
-                  className={styles.img_list}
-                  key={i}
-                  src={e["imgAddress"]}
-                  alt=""
-                />
-              ))}
-          </div>
-        </div>
-        <div className={styles.bottom_container}>
-          <h2>이런 분들께 추천해요</h2>
-          <div>{data.description}</div>
-        </div>
-
-        {memberId === data.masterId ? (
-          <div className={styles.primary} onClick={startChallengeHandler}>
-            챌린지 시작하기!
-            <div className={styles.shadow}></div>
-          </div>
-        ) : (
-          <div
-            style={{
-              position: "absolute",
-              bottom: "2rem",
-              left: "50%",
-              transform: "translateX(-50%)",
-            }}
-          >
-            {isJoin ? (
-              <div className={styles.primary2}>
-                이미 참여중이에요!
-                <div className={styles.shadow}></div>
-              </div>
-            ) : (
-              <div className={styles.primary} onClick={joinChallengeHandler}>
-                챌린지에 도전할래요!
-                <div className={styles.shadow}></div>
-              </div>
-            )}
-          </div>
-        )}
-
-        {makeToast && (
-          <Toast setToast={setMakeToast} text="시작 가능한 인원이 부족해요!" />
-        )}
-        {joinToast && (
-          <Toast setToast={setJoinToast} text="참여가 완료 되었어요!" />
-        )}
       </div>
+      <div className={styles.mid_container}>
+        <h2>이렇게 인증해주세요</h2>
+        <div>- 매일 전날의 소비 내역을 불러와주세요.</div>
+        <div>- 사진이 잘 보이도록 촬영해주세요.</div>
+        <div className={styles.auth_img}>
+          {data.challengeExampleImageDtoList &&
+            data.challengeExampleImageDtoList.map((e, i) => (
+              <img
+                className={styles.img_list}
+                key={i}
+                src={e["imgAddress"]}
+                alt=""
+              />
+            ))}
+        </div>
+      </div>
+      <div className={styles.bottom_container}>
+        <h2>이런 분들께 추천해요</h2>
+        <div>{data.description}</div>
+      </div>
+
+      {memberId === data.masterId ? (
+        <div className={styles.primary} onClick={startChallengeHandler}>
+          챌린지 시작하기!
+          <div className={styles.shadow}></div>
+        </div>
+      ) : (
+        <div
+          style={{
+            position: "absolute",
+            bottom: "2rem",
+            left: "50%",
+            transform: "translateX(-50%)",
+          }}
+        >
+          {isJoin ? (
+            <div className={styles.primary2}>
+              이미 참여중이에요!
+              <div className={styles.shadow}></div>
+            </div>
+          ) : (
+            <div className={styles.primary} onClick={joinChallengeHandler}>
+              챌린지에 도전할래요!
+              <div className={styles.shadow}></div>
+            </div>
+          )}
+        </div>
+      )}
+
+      {makeToast && (
+        <Toast setToast={setMakeToast} text="시작 가능한 인원이 부족해요!" />
+      )}
+      {joinToast && (
+        <Toast setToast={setJoinToast} text="참여가 완료 되었어요!" />
+      )}
     </div>
   );
 };
