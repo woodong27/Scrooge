7a6a177220fe381a6179ce418a096e6a09048215
이동주 <leednj0113@naver.com>
2023-08-18 09:50:00 +0900

    Fix: 웹 소켓 채팅 접속 시만 연결

--- scrooge-react/src/utils/WebSocketComponent.js
+++ scrooge-react/src/utils/WebSocketComponent.js
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useSelector } from "react-redux";
 import { useParams, Link } from "react-router-dom";
 import axios from "axios";
@@ -11,29 +11,37 @@ const WebSocketComponent = () => {
   const globalToken = useSelector((state) => state.globalToken);
   const msgRef = useRef();
   const params = useParams();
-  const socket = new WebSocket("ws:/day6scrooge.duckdns.org/api/ws");
-  const stompClient = Stomp.over(socket);
-  const [data, setData] = useState();
+  const socket = useRef(null);
+  const stompClient = useRef(null);
+  const [data, setData] = useState(null);
   const [msg, setMsg] = useState([]);
   const [txt, setTxt] = useState("");
-  const containerRef = useRef("");
+  const containerRef = useRef(null);
 
   useEffect(() => {
-    msgRef.current.focus();
-
-    axios
-      .get("https://day6scrooge.duckdns.org/api/member/info", {
-        headers: {
-          Authorization: globalToken,
-        },
-      })
-      .then((resp) => {
+    const fetchData = async () => {
+      try {
+        const resp = await axios.get(
+          "https://day6scrooge.duckdns.org/api/member/info",
+          {
+            headers: {
+              Authorization: globalToken,
+            },
+          }
+        );
         setData(resp.data);
-      })
-      .catch((e) => console.log(e));
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchData();
+
+    socket.current = new WebSocket("ws:/day6scrooge.duckdns.org/api/ws");
+    stompClient.current = Stomp.over(socket.current);
 
     const onConnect = () => {
-      stompClient.subscribe(
+      stompClient.current.subscribe(
         `/topic/chat/${params.id}/${params.team}`,
         (data) => {
           const parseData = JSON.parse(data.body);
@@ -49,19 +57,19 @@ const WebSocketComponent = () => {
       );
     };
 
-    stompClient.connect({}, onConnect);
+    stompClient.current.connect({}, onConnect);
 
     return () => {
-      stompClient.disconnect();
+      stompClient.current.disconnect();
     };
-  }, []);
+  }, [globalToken, params.id, params.team]);
 
   const sendMsgHandler = () => {
-    if (!socket.readyState) return;
+    if (!socket.current.readyState) return;
     msgRef.current.focus();
     setTxt("");
 
-    stompClient.send(
+    stompClient.current.send(
       `/app/chat/${params.id}/${params.team}`,
       {},
       JSON.stringify({ content: msgRef.current.value, sender: data.nickname })
@@ -72,29 +80,28 @@ const WebSocketComponent = () => {
     <div className={styles.bg}>
       <div className={styles.header}>
         <Link className={styles.back} to={`/challenge/my/${params.id}`}>
-          <img src={backImg} alt=""></img>
+          <img src={backImg} alt="" />
         </Link>
         <div className={styles.chat}>우리팀 채팅</div>
       </div>
       <div className={styles.body} ref={containerRef}>
-        {msg.map((e, i) =>
-          e.sender === data.nickname ? (
-            <div className={styles.me} key={i}>
-              <img
-                src={`${process.env.PUBLIC_URL}/images/you.png`}
-                alt=""
-              ></img>
-              <div className={styles.nickname}>나</div>
-              <div className={styles.text}>{e.content}</div>
+        {msg.map((e, i) => (
+          <div
+            className={e.sender === data.nickname ? styles.me : styles.you}
+            key={i}
+          >
+            <img
+              src={`${process.env.PUBLIC_URL}/images/${
+                e.sender === data.nickname ? "you.png" : "me.png"
+              }`}
+              alt=""
+            />
+            <div className={styles.nickname}>
+              {e.sender === data.nickname ? "나" : e.sender}
             </div>
-          ) : (
-            <div className={styles.you} key={i}>
-              <img src={`${process.env.PUBLIC_URL}/images/me.png`} alt=""></img>
-              <div className={styles.nickname}>{e.sender}</div>
-              <div className={styles.text}>{e.content}</div>
-            </div>
-          )
-        )}
+            <div className={styles.text}>{e.content}</div>
+          </div>
+        ))}
       </div>
       <div className={styles.input_layout}>
         <div className={styles.input_container}>
